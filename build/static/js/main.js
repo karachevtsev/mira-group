278d0079ea27de7edb936958ca6094ca0f09f479
karachevtsev <karachevtsev@yahoo.com>
2020-04-15 00:59:55 +0300

    Added about section, added scrollspy nabvbar, wip statistics, wip services

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -7,4 +7,17 @@ $(document).ready(function () {
     speed: 1000,
     fade: true
   });
+  var header = $('.js-header');
+  var headerOffset = header.offset();
+  var headerHeight = header.innerHeight();
+  var section = $('.hero');
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > headerOffset.top) {
+      header.addClass('fixed-top');
+      section.css('padding-top', headerHeight);
+    } else {
+      header.removeClass('fixed-top');
+      section.css('padding-top', 0);
+    }
+  });
 });
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -5,4 +5,17 @@ $(document).ready(function () {
     speed: 1000,
     fade: true
   })
+  const header = $('.js-header')
+  const headerOffset = header.offset()
+  const headerHeight = header.innerHeight()
+  const section = $('.hero')
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > headerOffset.top) {
+      header.addClass('fixed-top')
+      section.css('padding-top', headerHeight)
+    } else {
+      header.removeClass('fixed-top')
+      section.css('padding-top', 0)
+    }
+  })
 })
